e5771be2dca896cb1257cf0cd1540b58ceff54b4
Martin Janousek <mjanousek@eurofunk.com>
2019-11-11 20:02:06 +0100

    Priprava 12-11-2019

---
+++ 12-11-2019/obsah-hodiny.md
@@ -0,0 +1,4 @@
+# 12.11.2019
+
+- Test - zadani se nachazi v souboru test.md a test vypracovavejte do souboru test.php.
+- PHP superglobalni promenne a zpracovani formulare [prezentace](https://docs.google.com/presentation/d/16P5PqO-50L4_YXbcL5rsGA95CJvC3dzcu4NZPp0x-H0/edit?usp=sharing)
---
+++ 12-11-2019/test.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Test 12.11.</title>
+</head>
+<body>
+
+<?php
+
+// TODO: napsat funkci
+
+echo textLengthToString('ahoj'); // délka řetězce: 4
+echo '<br>';
+echo textLengthToString('ahoj', 'délka ahoj je'); // délka ahoj je: 4
+
+
+// TODO: napsat funkci
+
+echo '<br>';
+echo sumArray([['hodnota' => 1] , ['hodnota' => 2]]); // 3
+echo '<br>';
+echo sumArray([['hodnota' => 1] , ['hodnota' => 2], ['hodnota' => 3]]); // 6
+
+
+?>
+
+</body>
+</html>
