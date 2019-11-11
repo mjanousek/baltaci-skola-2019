3bf36823b6a73fb49ff6647020ce17095b89d90a
Martin Janousek <mjanousek@eurofunk.com>
2019-11-11 21:04:42 +0100

    Kalkulacka GET sablona

---
+++ 12-11-2019/kalkulacka-GET.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Test 12.11.</title>
+</head>
+
+<style>
+    div{
+        margin-bottom: 15px;
+    }
+    label{
+        margin-right: 15px;
+    }
+</style>
+
+<body>
+
+<form action="..." method="...">
+    <div>
+        <label for="value1">Cislo 1:</label>
+        <input type="text" name="...">
+    </div>
+    <div>
+        <label for="value2">Cislo 2:</label>
+        <input type="text" name="...">
+    </div>
+    <input type="submit" value="Secti cisla">
+</form>
+
+<?php
+
+// ...
+
+?>
+
+</body>
